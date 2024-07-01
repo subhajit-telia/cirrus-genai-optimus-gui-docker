5256da96c761697c304b4cdbcf81ddd7a1974726
Subhajit Manna <subhajit.manna@teliacompany.com>
2024-07-01 19:29:03 +0530

    sync

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ export default {
     content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
       colors: {
-        primary: 'var(--color-primary)'
+        primary: 'var(--color-primary)',
+        white: 'var(--color-white)'
       },
       extend: {},
     },
