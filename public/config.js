8f239d3ffde126ad423b542803e33b179db95ac6
Subhajit Manna <subhajit.manna@teliacompany.com>
2024-10-23 20:26:56 +0530

    refactoring changes

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.RUNTIME_ENV = {
-    REACT_APP_API_URL: "http://new-genai-optimus.cirrus-dev.teliacompany.net",
+    REACT_APP_API_URL: "https://new-genai-optimus.cirrus-dev.teliacompany.net/api/v1",
     REACT_APP_ENV: "development"
   };
   
