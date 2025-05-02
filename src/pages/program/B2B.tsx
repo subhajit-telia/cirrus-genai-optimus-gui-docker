6abf3526d5fb3883254511a19ea18e9e378a6c44
Subhajit Manna <subhajit.manna@teliacompany.com>
2025-05-02 14:47:35 +0530

    B2XSET-348757 - B2XSET-348828

--- src/pages/program/B2B.tsx
+++ src/pages/program/B2B.tsx
@@ -15,6 +15,7 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
 import ProductDropdown from '../../components/dropdown/productDropdown/ProductDropdown';
+import { Tooltip } from 'react-tooltip';
 
 
 type Tab = {
@@ -979,6 +980,14 @@ const B2B: React.FC = () => {
   };
   /* Self learning end */
 
+  const handleClickContentful = (e: React.MouseEvent<HTMLIonChipElement>) => {
+    console.log('handleClickContentful', e);
+    if (!(e.target as HTMLIonChipElement).disabled) {
+      setIsContentfulModal(true);
+    }
+    
+  };
+
   return (
     <IonPage>
       <AppHeader/>
@@ -1130,9 +1139,10 @@ const B2B: React.FC = () => {
                     <Tabs tabs={tabs} regenarateItem={regenarateItem} saveEditedAnswer={saveEditedAnswer} genarateRefineCopy={genarateRefineCopy} contentfulData={sendTocontentful} isEditingMode={handleEditingMode}/>
                     <div className="text-right mt-3">
                       <IonChip onClick={() => handleFormSubmit(requestData)} className='text-sm ml-2.5 mr-0 min-h-6 py-0 bg-white text-primary border-primary border-2 font-semibold rounded-lg'>Rewrite all suggestions</IonChip>
-                      <IonChip disabled={isOpenEditing} onClick={() => setIsContentfulModal(true)} className='text-sm ml-2.5 mr-0 min-h-6 py-0 bg-white text-primary border-primary border-2 font-semibold rounded-lg'>Send to contentful</IonChip>
+                      <IonChip data-tooltip-id="contentful" data-tooltip-content="Save all content before sending it to Contentful." disabled={isOpenEditing} onClick={ handleClickContentful } className='!pointer-events-auto text-sm ml-2.5 mr-0 min-h-6 py-0 bg-white text-primary border-primary border-2 font-semibold rounded-lg'>Send to contentful</IonChip>
                       <IonChip onClick={() => exportToDoc(tabs)} className='text-sm ml-2.5 mr-0 min-h-6 py-0 bg-white text-primary border-primary border-2 font-semibold rounded-lg'>Save all suggestions to word.doc</IonChip>
                       {/* <IonChip onClick={handleReset} className='text-sm ml-2.5 mr-0 min-h-6 py-0 bg-white text-primary border-primary border-2 font-semibold rounded-lg'>Create new task</IonChip> */}
+                      <Tooltip className={`${!isOpenEditing ? 'hidden' : ''}`} id="contentful" />
                     </div>
                   </div>
                 </IonCol>
