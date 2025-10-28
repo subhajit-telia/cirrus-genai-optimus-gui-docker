f51b71e96fce2e00a8388f9f75773d89d354f179
Subhajit Manna <subhajit.manna@teliacompany.com>
2025-10-28 13:06:34 +0530

    calling graph api

--- src/components/loginbutton/LoginButton.tsx
+++ src/components/loginbutton/LoginButton.tsx
@@ -36,7 +36,9 @@ const LoginButton = () => {
 
       // Store "removed" for API calls
       const accessToken = response.accessToken;
+      
       if (accessToken) {
+        getUserDetails(accessToken);
         const groups = ((response.idTokenClaims as { groups?: string[] })?.groups || []).filter(
           (group) => group.includes('OPTIMUS') || group.includes('KNOWLEDGEBASE')
         );
@@ -68,6 +70,31 @@ const LoginButton = () => {
       console.error("Login Failed:", error);
     }
   };
+
+  /* -------------get user details data start------------- */
+  const getUserDetails = async (accessToken:string) => {
+    try {
+      const urlData = 'https://graph.microsoft.com/v1.0/me/memberOf';
+
+      const response = await fetch(urlData, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      
+      const responseData = await response.json();
+      console.log("Success graph data:", responseData);
+
+      if (response.ok) {
+
+      }
+      
+    } catch (error: any) {
+      console.error("catch failed:", error);
+    }
+  };
+  /* get user details data end */
 
   return <IonSegmentButton onClick={handleLogin} className='size-min min-w-0 h-7 min-h-6' value="sso"><IonLabel  className='m-0 text-xs'>AZURE AD</IonLabel></IonSegmentButton>;
 };
