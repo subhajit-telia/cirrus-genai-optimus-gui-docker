3cd5ed4d03b91371f957ac353c9bf6c4970d4c91
Subhajit Manna <subhajit.manna@teliacompany.com>
2025-12-05 18:04:04 +0530

    B2XSET-377601 2

--- src/config/msalConfig.ts
+++ src/config/msalConfig.ts
@@ -7,11 +7,7 @@ console.log("Current URL:", currentUrl);
 const RUNTIME_ENV = (window as any).RUNTIME_ENV || {};
 console.log("RUNTIME_ENV:", RUNTIME_ENV);
 
-console.log("MSAL Config Values:", {
-  clientId: process.env.AZURE_AD_CLIENT_ID,
-  clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
-  tenantId: process.env.AZURE_AD_TENANT_ID,
-});
+
 
 
 const msalConfig = {
