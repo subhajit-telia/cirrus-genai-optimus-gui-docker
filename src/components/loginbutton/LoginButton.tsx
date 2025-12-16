963dc5ffd4ae800caadf8bb8ae30669f6c2bca19
Subhajit Manna <subhajit.manna@teliacompany.com>
2025-12-16 20:29:45 +0530

    test Azure login without using AZURE_SECRET_ID

--- src/components/loginbutton/LoginButton.tsx
+++ src/components/loginbutton/LoginButton.tsx
@@ -13,9 +13,6 @@ const LoginButton = () => {
   const handleLogin = async () => {
     const currentUrl = window.location.href;
     console.log("Current URL:", currentUrl);
-    console.log("AZURE_CLIENT_ID:", NetworkInfo.AZURE_CLIENT_ID);
-    console.log("AZURE_TENANT_ID:", NetworkInfo.AZURE_TENANT_ID);
-    console.log("AZURE_SECRET_ID:", NetworkInfo.AZURE_SECRET_ID);
     console.log("ACCESSTOKEN:", NetworkInfo.ACCESSTOKEN);
     console.log("REACT_APP_URL:", NetworkInfo.REACT_APP_URL);
     console.log("URL:", NetworkInfo.URL);
--- src/config/msalConfig.ts
+++ src/config/msalConfig.ts
@@ -7,15 +7,23 @@ console.log("Current URL:", currentUrl);
 const RUNTIME_ENV = (window as any).RUNTIME_ENV || {};
 console.log("RUNTIME_ENV:", RUNTIME_ENV);
 
+const AZURE_TENANT_ID = '05764a73-8c6f-4538-83cd-413f1e1b5665';
+let AZURE_CLIENT_ID;
+if (currentUrl.toLowerCase().includes('dev')) {
+  AZURE_CLIENT_ID = 'a8a6ba07-6881-4c2a-aa0e-cf5f09e64fe5';
+}else if (currentUrl.toLowerCase().includes('stage')) {
+  AZURE_CLIENT_ID = 'd869b7d7-c404-430d-8b5d-a9d8d2cc0bba';
+}else {
+  AZURE_CLIENT_ID = '2e0ed0b0-05a6-46c1-8fde-f50fc6dbdf9c';
+}
 
 
 
 const msalConfig = {
   auth: {
-    clientId: `${NetworkInfo.AZURE_CLIENT_ID}`, // Application (client) ID from Azure
-    authority: `https://login.microsoftonline.com/${NetworkInfo.AZURE_TENANT_ID}`, // Directory (tenant) ID from Azure
+    clientId: AZURE_CLIENT_ID, // Application (client) ID from Azure
+    authority: `https://login.microsoftonline.com/${AZURE_TENANT_ID}`, // Directory (tenant) ID from Azure
     // redirectUri: "http://localhost:8100/use-cases",
-    clientSecret: NetworkInfo.AZURE_SECRET_ID,
   },
   cache: {
     cacheLocation: "sessionStorage",
