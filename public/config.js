fc04ecf77ff2c88fa867ce051333f8afcfc64a30
Subhajit Manna <subhajit.manna@teliacompany.com>
2025-12-16 20:29:45 +0530

    test Azure login without using AZURE_SECRET_ID

--- public/config.js
+++ public/config.js
@@ -2,8 +2,5 @@ window.RUNTIME_ENV = {
     REACT_APP_API_URL: "http://0.0.0.0:5000/api/v1",
     // REACT_APP_API_URL: "https://new-genai-optimus.cirrus-dev.teliacompany.net/api/v1",
     REACT_APP_ENV: "development",
-    AZURE_AD_CLIENT_ID: "${AZURE_AD_CLIENT_ID}",
-    AZURE_AD_CLIENT_SECRET: "${AZURE_AD_CLIENT_SECRET}",
-    AZURE_AD_TENANT_ID: "${AZURE_AD_TENANT_ID}",
   };
   
