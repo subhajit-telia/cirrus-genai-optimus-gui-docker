7b065a752dc86eadca24e2d3ffc3390840a78b7b
subhajit-telia <subhajit.manna@teliacompany.com>
2026-03-20 19:47:43 +0530

    B2XSET-376557 Test

--- public/config.js
+++ public/config.js
@@ -1,6 +1,8 @@
 window.RUNTIME_ENV = {
-    REACT_APP_API_URL: "http://0.0.0.0:5000/api/v1",
+    REACT_APP_API_URL: "__REACT_APP_API_URL__",
     // REACT_APP_API_URL: "https://new-genai-optimus.cirrus-dev.teliacompany.net/api/v1",
-    REACT_APP_ENV: "development",
+    REACT_APP_ENV: "__REACT_APP_ENV__",
+    AZURE_CLIENT_ID: "__AZURE_CLIENT_ID__",
+    AZURE_TENANT_ID: "__AZURE_TENANT_ID__",
   };
   
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -5,6 +5,9 @@ declare global {
   interface Window {
     RUNTIME_ENV: {
       REACT_APP_API_URL: string;
+      REACT_APP_ENV: string;
+      AZURE_CLIENT_ID: string;
+      AZURE_TENANT_ID: string;
     };
   }
 }
