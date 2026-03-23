d45292215fdea78ca8a262f7780ff1b6eb17dba3
subhajit-telia <subhajit.manna@teliacompany.com>
2026-03-23 15:50:44 +0530

    Change for SSO login

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ CONFIG_JS_PATH="/app/public/config.js"
 
 # Replace placeholders in config.json
 if [ -f "$CONFIG_JS_PATH" ]; then
-    sed -i "s|__REACT_APP_API_URL__|${REACT_APP_API_URL}|g" "$CONFIG_JS_PATH"
     sed -i "s|__REACT_APP_ENV__|${REACT_APP_ENV}|g" "$CONFIG_JS_PATH"
     sed -i "s|__AZURE_CLIENT_ID__|${AZURE_CLIENT_ID}|g" "$CONFIG_JS_PATH"
     sed -i "s|__AZURE_TENANT_ID__|${AZURE_TENANT_ID}|g" "$CONFIG_JS_PATH"
