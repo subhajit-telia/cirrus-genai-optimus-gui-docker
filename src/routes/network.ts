ac00abf4d4a5ff4389e638377fb2978e6d79ce2d
subhajit-telia <subhajit.manna@teliacompany.com>
2026-03-26 18:38:53 +0530

    Revert previous changes

--- src/routes/network.ts
+++ src/routes/network.ts
@@ -15,7 +15,7 @@ class NetworkInfo {
   static readonly SANITIZE_URL: string =
     (NetworkInfo.runtimeEnv().API_ENDPOINT || import.meta.env.VITE_API_BASE_URL) + "/api";
 
-  static readonly URL: string = NetworkInfo.runtimeEnv().API_ENDPOINT;
+  static readonly URL: string = NetworkInfo.SANITIZE_URL.replace("undefined/", "");
 
   static readonly ACCESSTOKEN: string =
     NetworkInfo.runtimeEnv().API_KEY || import.meta.env.REMOVED;
