35721e250bf8f6264bc05e7a7789670d39aa1dd9
subhajit-telia <subhajit.manna@teliacompany.com>
2026-04-01 15:01:57 +0530

    Fix api endpoint 2

--- src/components/loginbutton/LoginButton.tsx
+++ src/components/loginbutton/LoginButton.tsx
@@ -14,6 +14,7 @@ const LoginButton = () => {
     const currentUrl = window.location.href;
     console.log("Current URL:", currentUrl);
     console.log("ACCESSTOKEN:", NetworkInfo.ACCESSTOKEN);
+    console.log("AZURE_AD_TENANT_ID:", NetworkInfo.AZURE_TENANT_ID);
     console.log("REACT_APP_API_URL:", NetworkInfo.REACT_APP_API_URL);
     console.log("URL:", NetworkInfo.URL);
     let reactAppUrl;
