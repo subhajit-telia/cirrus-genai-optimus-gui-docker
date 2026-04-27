41e0fdfb004427a1986b1f62ed5408f83797b697
subhajit-telia <subhajit.manna@teliacompany.com>
2026-04-27 19:58:56 +0530

    Fix login group issue in Azure

--- src/components/loginbutton/LoginButton.tsx
+++ src/components/loginbutton/LoginButton.tsx
@@ -4,12 +4,14 @@ import { IonButton, IonLabel, IonSegmentButton } from "@ionic/react";
 import { useHistory } from "react-router-dom";
 import { useAuth } from "../../config/AuthContext";
 import { NetworkInfo } from "../../routes/network";
+import { useState } from "react";
 
 const LoginButton = () => {
   const { instance } = useMsal();
   const history = useHistory();
   const { login } = useAuth();
-
+  
+  let account: any = null;
   const handleLogin = async () => {
     const currentUrl = window.location.href;
     console.log("Current URL:", currentUrl);
@@ -34,39 +36,18 @@ const LoginButton = () => {
       console.log("Login Response:", response);
 
       // Extract user details
-      const account = response.account;
+      account = response.account;
       console.log("User Info:", account?.name, account?.username);
 
       // Store access_token for API calls
       const accessToken = response.accessToken;
       
       if (accessToken) {
-        getUserDetails(accessToken);
+        getUserGraphDetails(accessToken);
         const groups = ((response.idTokenClaims as { groups?: string[] })?.groups || []).filter(
           (group) => group.includes('OPTIMUS') || group.includes('KNOWLEDGEBASE')
         );
-        const isUser = groups.includes('HID100007708_PROD_CIRRUS_GENAI_OPTIMUS_USER');
-        const isAdmin = groups.includes('HID100007708_PROD_CIRRUS_GENAI_OPTIMUS_ADMIN');
-        const userData = {
-          verification: true,
-          roles: {
-            user: isUser || true,
-            admin: isAdmin
-          },
-          tiga_roles: groups || [],
-          username: account?.username,
-          display_name: account?.name
-        };
-        localStorage.setItem('user', JSON.stringify(userData));
-
-        if (userData.roles.admin === true && userData.roles.user === true) {
-          login('admin');
-        }else if (userData.roles.admin === true) {
-          login('admin');
-        }else if (userData.roles.user === true) {
-          login('user');
-        }
-        history.push('/b2c');
+        console.log("User Groups from ID Token:", groups);
       }
       console.log("Access Token:", accessToken);
     } catch (error) {
@@ -74,30 +55,118 @@ const LoginButton = () => {
     }
   };
 
-  /* -------------get user details data start------------- */
-  const getUserDetails = async (accessToken:string) => {
+  const getUserGraphDetails = async (accessToken:string) => {
     try {
-      const urlData = 'https://graph.microsoft.com/v1.0/me';
+      const urlData = 'https://graph.microsoft.com/v1.0/me/getMemberObjects';
 
       const response = await fetch(urlData, {
-        method: 'GET',
+        method: 'POST',
         headers: {
           Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json"
         },
+        body: JSON.stringify({
+          securityEnabledOnly: false
+        }),
       });
       
       const responseData = await response.json();
-      console.log("Success graph data:", responseData);
-
+      console.log("Success graph:", responseData);
+      
       if (response.ok) {
-
+        getUserGraphIdDetails(responseData.value, accessToken);
       }
       
     } catch (error: any) {
       console.error("catch failed:", error);
     }
   };
+
+  const getUserGraphIdDetails = async (groupIds:Array<string>, accessToken:string) => {
+    try {
+      const urlData = 'https://graph.microsoft.com/v1.0/directoryObjects/getByIds';
+
+      const response = await fetch(urlData, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          ids: groupIds,
+          types: ["group"]
+        }),
+      });
+      
+      const responseData = await response.json();
+      console.log("Success graph id:", responseData);
+
+      if (response.ok) {
+        const group_names = responseData.value.map((g: any) => g.displayName);
+        console.log("Group Names:", group_names);
+        const filteredGroups = group_names.filter(
+          (group: string) => group.toUpperCase().includes('OPTIMUS') || group.toUpperCase().includes('CIRRUS_GENAI')
+        );
+        console.log("Filtered Groups>:", filteredGroups);
+
+        if (filteredGroups.length > 0) {
+          const isUser = filteredGroups.some((group: string) => group.toUpperCase() === 'HID100007708_PROD_CIRRUS_GENAI_OPTIMUS_USER');
+          const isAdmin = filteredGroups.some((group: string) => group.toUpperCase() === 'HID100007708_PROD_CIRRUS_GENAI_OPTIMUS_ADMIN');
+          const userData = {
+            verification: true,
+            roles: {
+              user: isUser || true,
+              admin: isAdmin
+            },
+            tiga_roles: filteredGroups || [],
+            username: account?.username,
+            display_name: account?.name
+          };
+          console.log("User Data to Store:", userData);
+          localStorage.setItem('user', JSON.stringify(userData));
+
+          if (userData.roles.admin === true && userData.roles.user === true) {
+            login('admin');
+          }else if (userData.roles.admin === true) {
+            login('admin');
+          }else if (userData.roles.user === true) {
+            login('user');
+          }
+          history.push('/b2c');
+        }
+      }
+    } catch (error: any) {
+      console.error("catch failed:", error);
+    }
+  };
   /* get user details data end */
+
+
+  /* -------------get user details data start------------- */
+  // const getUserDetails = async (accessToken:string) => {
+  //   try {
+  //     const urlData = 'https://graph.microsoft.com/v1.0/me';
+
+  //     const response = await fetch(urlData, {
+  //       method: 'GET',
+  //       headers: {
+  //         Authorization: `Bearer ${accessToken}`,
+  //       },
+  //     });
+      
+  //     const responseData = await response.json();
+  //     console.log("Success graph data:", responseData);
+
+  //     if (response.ok) {
+
+  //     }
+      
+  //   } catch (error: any) {
+  //     console.error("catch failed:", error);
+  //   }
+  // };
+
+
   // return <IonSegmentButton onClick={handleLogin} className='hidden size-min min-w-0 h-7 min-h-6' value="sso"><IonLabel  className='m-0 text-xs'>AZURE AD</IonLabel></IonSegmentButton>;
   return <IonButton className='btn-primary' shape="round" onClick={handleLogin}>Telia Single Sign-On</IonButton>;
 };
