5e8233696303d43ea4989d551835d8f4cebcd072
subhajit-telia <subhajit.manna@teliacompany.com>
2026-05-06 22:07:25 +0530

    Add msal config

--- src/config/msalConfig.ts
+++ src/config/msalConfig.ts
@@ -1,29 +1,9 @@
 import { PublicClientApplication } from "@azure/msal-browser";
 import { NetworkInfo } from "../routes/network";
-
-console.log("env>>", import.meta.env.VITE_API_BASE_URL);
-console.log("NetworkInfo>>", NetworkInfo.AZURE_CLIENT_ID);
-const currentUrl = window.location.href;
-console.log("Current URL:", currentUrl);
-const RUNTIME_ENV = (window as any).RUNTIME_ENV || {};
-console.log("RUNTIME_ENV:", RUNTIME_ENV);
-
-const AZURE_TENANT_ID = '05764a73-8c6f-4538-83cd-413f1e1b5665';
-let AZURE_CLIENT_ID;
-if (currentUrl.toLowerCase().includes('dev')) {
-  AZURE_CLIENT_ID = 'REMOVED';
-}else if (currentUrl.toLowerCase().includes('stage')) {
-  AZURE_CLIENT_ID = 'REMOVED';
-}else {
-  AZURE_CLIENT_ID = 'REMOVED';
-}
-
-
-
 const msalConfig = {
   auth: {
-    clientId: AZURE_CLIENT_ID, // Application (client) ID from Azure
-    authority: `https://login.microsoftonline.com/${AZURE_TENANT_ID}`, // Directory (tenant) ID from Azure
+    clientId: NetworkInfo.AZURE_CLIENT_ID, // from window.RUNTIME_ENV at runtime
+    authority: `https://login.microsoftonline.com/${NetworkInfo.AZURE_TENANT_ID}`, // from window.RUNTIME_ENV at runtime
     // redirectUri: "http://localhost:8100/use-cases",
   },
   cache: {
